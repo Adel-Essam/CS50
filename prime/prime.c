0a9ebd83e2ef25555317dbc187bf62bef0441728
Adel Essam <122894217+Adel-Essam@users.noreply.github.com>
2023-07-29 11:39:12 +0000

    Sat, Jul 29, 2023, 1:39 PM +02:00

--- prime/prime.c
+++ prime/prime.c
@@ -16,9 +16,9 @@ int main(void)
     do
     {
         max = get_int("Maximum: ");
-    } 
+    }
     while (min >= max);
-    
+
     for (int i = min; i <= max; i++)
     {
         if (prime(i))
@@ -30,6 +30,17 @@ int main(void)
 
 bool prime(int number)
 {
-    // TODO
-    return false;
+    if (number == 1 || number == 0)
+    {
+        return false;
+    }
+
+    for (int i = 2; i <= number / 2; i++)
+    {
+        if (number % i == 0 )
+        {
+            return false;
+            break;
+        }
+    }
 }
