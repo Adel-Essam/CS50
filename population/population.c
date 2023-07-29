86c04577eb121fb0718e9f41a7eb1dae64ed926f
Adel Essam <122894217+Adel-Essam@users.noreply.github.com>
2023-07-29 15:41:21 +0000

    Sat, Jul 29, 2023, 5:41 PM +02:00

--- population/population.c
+++ population/population.c
@@ -4,10 +4,35 @@
 int main(void)
 {
     // TODO: Prompt for start size
+    int start;
+    do
+    {
+        start = get_int("Start size: ");
+    }
+    while (start < 9);
 
     // TODO: Prompt for end size
+    int end;
+    do
+    {
+        end = get_int("End size: ");
+    }
+    while (end <= start);
+
 
     // TODO: Calculate number of years until we reach threshold
-
+    int n = 0;
+    for(int i = 0;;i++)
+    {
+        if(start >= end)
+        {
+            break;
+        }
+        start += (start / 3 - start / 4);
+        // printf(" %i \n", start);
+        n++;
+    }
     // TODO: Print number of years
+    printf("years: %i", n);
 }
+
