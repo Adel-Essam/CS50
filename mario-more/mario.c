b3e6ff0b4f7361368ed84ec796ab39af76f9b706
Adel Essam <122894217+Adel-Essam@users.noreply.github.com>
2023-07-29 16:34:14 +0000

    Sat, Jul 29, 2023, 6:34 PM +02:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,7 +1,35 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+        int n;
+    do
+    {
+        n = get_int("hight: ");
+    }
+    while (n > 8 || n < 1);
+
+    int x = n;
+    for (int i = 0;i < n;i++)
+    {
+        for(int l = x;l >= 0;l--)
+        {
+            printf(" ");
+        }
+        x--;
+
+        for (int j = 0;j <= i;j++)
+        {
+            printf("#");
+        }
+            printf("  ");
+        for (int k = 0;k <= i;k++)
+        {
+            printf("#");
+        }
+
+            printf("\n");
+    }
+
 }
