493c3b2dbfc0b39fa901943cf8517774e0b1af1e
Adel Essam <122894217+Adel-Essam@users.noreply.github.com>
2023-08-02 23:04:31 +0000

    Thu, Aug 3, 2023, 1:04 AM +02:00

--- no-vowels/no-vowels.c
+++ no-vowels/no-vowels.c
@@ -5,9 +5,42 @@
 
 #include <cs50.h>
 #include <stdio.h>
-#include <string>
+#include <string.h>
+
+void replace(int n, string s []);
 
 int main(int argc, string argv[])
 {
+    printf("usage: ./no-vowels word");
+    if (argc != 2)
+    {
+        printf("error there must be one word");
+        return 1;
+    }
+    replace(argv[1]);
+
+    printf("%s/n"argv[i]);
+
+}
+
+void replace(string s[])
+{
+    for (int i = 0; i != '/0'; i++)
+    {
+        switch ((int) s[i])
+        {
+        case 97:
+            s[i] = '6';
+        case 101:
+            s[i] = '3';
+        case 105:
+            s[i] = '1';
+        case 111:
+            s[i] = '0';
+
+        default:
+            continue;
+        }
+    }
 
 }
