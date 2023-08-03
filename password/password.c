b7208a8532ddd28563111849799630ee9b13f7a0
Adel Essam <122894217+Adel-Essam@users.noreply.github.com>
2023-08-03 00:21:42 +0000

    Thu, Aug 3, 2023, 2:21 AM +02:00

--- password/password.c
+++ password/password.c
@@ -23,5 +23,22 @@ int main(void)
 // TODO: Complete the Boolean function below
 bool valid(string password)
 {
-    return false;
+    int n = strlen(password);
+    bool b0 = 0, b1 = 0, b2 = 0;
+    for (int i = 0; i < n; i++)
+        {
+            if((int) password[i] >=33 && (int) password[i] <=57)
+            {
+                b0 = 1;
+            }
+            if((int) password[i] >=65 && (int) password[i] <= 90)
+            {
+                b1 = 1;
+            }
+            if((int) password[i] >=97 && (int) password[i] <=122)
+            {
+                b2 = 1;
+            }
+        }
+        return (b0 && b1 && b2);
 }
