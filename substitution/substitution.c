cf4106a6aee2f14a0f092b0fbe89f37e26a6d8ff
Adel Essam <122894217+Adel-Essam@users.noreply.github.com>
2023-08-11 09:19:21 +0000

    Fri, Aug 11, 2023, 11:19 AM +02:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,55 +1,52 @@
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
 
 int main(int argc, string argv[])
 {
-    string s = get_string(0, "code: ");
     string s2 = get_string(0, "message: ");
     char arr[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-                    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-    int se = strlen(s);
+                  'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    int se = strlen(argv[1]);
 
-    for (int co = 0; argv[1][co] !='\0'; co++)
+    for (int co = 0; argv[1][co] != '\0'; co++)
     {
-        if (!(isalpha(s[1][co])) || se == 0)
+        if (!(isalpha(argv[1][co])) || se == 0)
         {
             printf("Usage: ./substitution key\n");
             return 0;
         }
     }
-        if (se < 26)
-        {
-            printf("Key must contain 26 characters.\n");
-        }
+    if (se < 26)
+    {
+        printf("Key must contain 26 characters.\n");
+    }
 
     for (int i = 0; s2[i] != '\0'; i++)
     {
         for (int j = 0; j < 26; j++)
         {
-            //chech if the character in the word is in the array of chars
+            // chech if the character in the word is in the array of chars
             if (tolower(s2[i]) == arr[j])
             {
-                //check if the letter is upper case or lower case to print it
+                // check if the letter is upper case or lower case to print it
                 if (isupper(s2[i]))
                 {
-                    printf("%c", toupper(s[j]));
+                    printf("%c", toupper(argv[1][j]));
                 }
                 else
                 {
-                    printf("%c", tolower(s[j]));
+                    printf("%c", tolower(argv[1][j]));
                 }
                 break;
             }
-            //check if there is any punctuation marks or spaces
+            // check if there is any punctuation marks or spaces
             else if (isalpha(s2[i]) == 0)
             {
-                printf("%c",s2[i]);
+                printf("%c", s2[i]);
                 break;
             }
         }
     }
-}
-
 }
