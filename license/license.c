e9c123684ae77b73a87e6d7e136723be83a694f7
Adel Essam <122894217+Adel-Essam@users.noreply.github.com>
2023-08-27 14:12:28 +0000

    Sun, Aug 27, 2023, 4:12 PM +02:00

--- license/license.c
+++ license/license.c
@@ -26,13 +26,13 @@ int main(int argc, char *argv[])
 
         // Save plate number in array
         plates[idx] = buffer;
-        // printf("%s\n", buffer);
+        printf("%s\n", plates[idx]);
         idx++;
     }
 
-    for (int i = 0; i < 8; i++)
-    {
-        printf("%s\n", plates[i]);
-    }
+    // for (int i = 0; i < 8; i++)
+    // {
+    //     printf("%s\n", plates[i]);
+    // }
     fclose(infile);
 }
