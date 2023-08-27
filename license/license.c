40e295e2a80e3aeb5f823b1a8eefee53ae295937
Adel Essam <122894217+Adel-Essam@users.noreply.github.com>
2023-08-27 14:06:10 +0000

    Sun, Aug 27, 2023, 4:06 PM +02:00

--- license/license.c
+++ license/license.c
@@ -30,9 +30,9 @@ int main(int argc, char *argv[])
         idx++;
     }
 
-    // for (int i = 0; i < 8; i++)
-    // {
-    //     printf("%s\n", plates[i]);
-    // }
+    for (int i = 0; i < 8; i++)
+    {
+        printf("%s\n", plates[i]);
+    }
     fclose(infile);
 }
