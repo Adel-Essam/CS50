a4f2b4d9d2417af61eca30942695d4eeee875600
Adel Essam <122894217+Adel-Essam@users.noreply.github.com>
2023-08-30 00:46:06 +0000

    Wed, Aug 30, 2023, 2:46 AM +02:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -2,6 +2,15 @@
 
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
-    RGBTRIPLE.rgbtBlue
-    // Change all black pixels to a color of your choosing
+    for (int i =  0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (RGBTRIPLE[i][j].rgbtBlue == 0x00)
+        }
+    }
+    // RGBTRIPLE.rgbtBlue
+    // RGBTRIPLE.rgbtRed
+    // RGBTRIPLE.rgbtGreen
+
 }
