8b829c749d31abf0f9d204a8d3343ed782a22e6a
Adel Essam <122894217+Adel-Essam@users.noreply.github.com>
2023-09-23 17:26:08 +0000

    Sat, Sep 23, 2023, 7:26 PM +02:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -5,9 +5,31 @@ import sys
 figlet = Figlet()
 
 figlet.getFonts()
-f = sys.argv[2]
-figlet.setFont(font=f)
+if len(argv) < 3 and len(argv) != 1 :
+        print("Invalid usage length")
+        print(len(argv))
+        exit(1)
 
-s = input("Input: ")
-print("Output: ")
-print(figlet.renderText(s))
+    elif len(argv) == 3:
+        if  argv[1] != '-f':
+            print("Invalid usage flag")
+            exit(1)
+
+        try:
+            fo = argv[2]
+
+            if figlet.setFont(font = fo) == None:
+                text = input("Text: ")
+                output = figlet.renderText(text)
+                print("Output:")
+                print(output)
+
+        except:
+            print("Invalid usage")
+    elif len(argv) == 1:
+
+        figlet.setFont(font = random.choice(["slant", "rectangles", "digital", "slant", "alphabet", "epic", "isometric3", "roman"]))
+        text = input("Text: ")
+        output = figlet.renderText(text)
+        print("Output:")
+        print(output)
