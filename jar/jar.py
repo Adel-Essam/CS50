46faae9bc954a9cfc44ff99adbb8b4dc4881831b
Adel Essam <122894217+Adel-Essam@users.noreply.github.com>
2023-09-23 22:05:07 +0000

    Sun, Sep 24, 2023, 12:05 AM +02:00

--- jar/jar.py
+++ jar/jar.py
@@ -1,20 +1,33 @@
 class Jar:
-    def __init__(self, capacity=12):
-        ...
+    def __init__(self, capacity = 12):
+        self.cap = capacity
+        self.cap2 = capacity
 
     def __str__(self):
-        ...
-
+        return "🍪"*self.cap
     def deposit(self, n):
-        ...
+        self.cap+= n
 
     def withdraw(self, n):
-        ...
+        self.cap -= n
 
-    @property
     def capacity(self):
-        ...
-
-    @property
+        return self.cap2
     def size(self):
-        ...
+        return self.cap
+
+def main():
+
+    jar1 = Jar(10)
+    print(jar1.__str__())
+
+    jar1.withdraw(2)
+    print(jar1.__str__())
+
+    jar1.deposit(5)
+    print(jar1.__str__())
+
+    print(jar1.capacity())
+    print(jar1.size())
+
+main()
