1e3531089eca5983ad4b516510b477a4d6aec37c
Adel Essam <122894217+Adel-Essam@users.noreply.github.com>
2023-09-23 17:27:10 +0000

    Sat, Sep 23, 2023, 7:27 PM +02:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -6,30 +6,30 @@ figlet = Figlet()
 
 figlet.getFonts()
 if len(argv) < 3 and len(argv) != 1 :
-        print("Invalid usage length")
-        print(len(argv))
+    print("Invalid usage length")
+    print(len(argv))
+    exit(1)
+
+elif len(argv) == 3:
+    if  argv[1] != '-f':
+        print("Invalid usage flag")
         exit(1)
 
-    elif len(argv) == 3:
-        if  argv[1] != '-f':
-            print("Invalid usage flag")
-            exit(1)
+    try:
+        fo = argv[2]
 
-        try:
-            fo = argv[2]
+        if figlet.setFont(font = fo) == None:
+            text = input("Text: ")
+            output = figlet.renderText(text)
+            print("Output:")
+            print(output)
 
-            if figlet.setFont(font = fo) == None:
-                text = input("Text: ")
-                output = figlet.renderText(text)
-                print("Output:")
-                print(output)
+    except:
+        print("Invalid usage")
+elif len(argv) == 1:
 
-        except:
-            print("Invalid usage")
-    elif len(argv) == 1:
-
-        figlet.setFont(font = random.choice(["slant", "rectangles", "digital", "slant", "alphabet", "epic", "isometric3", "roman"]))
-        text = input("Text: ")
-        output = figlet.renderText(text)
-        print("Output:")
-        print(output)
+    figlet.setFont(font = random.choice(["slant", "rectangles", "digital", "slant", "alphabet", "epic", "isometric3", "roman"]))
+    text = input("Text: ")
+    output = figlet.renderText(text)
+    print("Output:")
+    print(output)
