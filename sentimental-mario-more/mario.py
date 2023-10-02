fbbdfaeb1ebf043bcc7e92a85c52ca2fe14a12ab
Adel Essam <122894217+Adel-Essam@users.noreply.github.com>
2023-10-02 15:32:46 +0000

    Mon, Oct 2, 2023, 5:32 PM +02:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -3,12 +3,11 @@ def main():
     n = int(input("Height: "))
     if n < 1:
         print("Enter a positive value")
-        re
-    elif n == 1:
-        print("#"+ "  " + "#")
+        return 1
     l = n
     for i in range(1, n + 1):
-        print(" " * l, end="")
+        if l > 1:
+            print(" " * (l - 1), end="")
         print("#" * i + "  " + "#" * i)
         l -= 1
 
