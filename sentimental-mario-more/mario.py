9d9c83543907909ba9c742c01eff8c6087dc4198
Adel Essam <122894217+Adel-Essam@users.noreply.github.com>
2023-10-02 15:00:15 +0000

    Mon, Oct 2, 2023, 5:00 PM +02:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,6 +1,11 @@
 # TODO
+import sys
 def main():
     n = int(input("Height: "))
+    if n < 1:
+        sys.exit(1)
+    elif n == 1:
+        print("#"+ "  " + "#")
     l = n
     for i in range(1, n + 1):
         print(" " * l, end="")
