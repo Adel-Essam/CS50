0bba9631cb3e6f5e9a8e431187a96b771732ed8e
Adel Essam <122894217+Adel-Essam@users.noreply.github.com>
2023-10-02 19:58:42 +0000

    Mon, Oct 2, 2023, 9:58 PM +02:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -3,8 +3,31 @@ def main():
     n = input("Number: ")
     le = len(n)
     n = int(n)
-    for i in range(n):
-        ...
+    sum1 = 0
+    sum2 = 0
+    check = 0
+    for i in range(le):
+        y = n % 10
+        if i % 2 == 1:
+            sum1 += int((y * 2) % 10)
+            sum1 += int((y * 2) / 10)
+        else:
+            sum2 += n % 10
+        if i == le - 2:
+            check = n
+        n /= 10
+        n = int(n)
+    if (sum1 + sum2) % 10 == 0:
+        if (check >= 51 and check <= 55) and le == 16:
+            print("MASTERCARD")
+        elif (check == 34 or check == 37) and le == 15:
+            print("AMEX")
+        elif (check / 10 == 4) and le == 13 or le == 16:
+            print("VISA")
+        else:
+            print("INVALID")
+    else:
+        print("INVALID")
 
 
 main()
