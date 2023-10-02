c5cf7696caa0404d0cb4c51adee66c1243950bf1
Adel Essam <122894217+Adel-Essam@users.noreply.github.com>
2023-10-02 15:52:19 +0000

    Mon, Oct 2, 2023, 5:52 PM +02:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,18 +1,19 @@
 # TODO
-from cs50 import get_int as geti
-
-
 def main():
-    n = geti("Height: ")
-    if n < 1:
-        print("Enter a positive value")
-        return 1
-    l = n
-    for i in range(1, n + 1):
-        if l > 1:
-            print(" " * (l - 1), end="")
-        print("#" * i + "  " + "#" * i)
-        l -= 1
+    while True:
+        try:
+            n = int(input("Height: "))
+            if int(n) and (n >= 1 and n <= 8):
+                l = n
+                for i in range(1, n + 1):
+                    if l > 1:
+                        print(" " * (l - 1), end="")
+                    print("#" * i + "  " + "#" * i)
+                    l -= 1
+                if i == n:
+                    break
+        except :
+                ...
 
 
 main()
