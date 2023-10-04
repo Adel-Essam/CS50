7e64f71846d274761cdd30e1c5967e92dc88f228
Adel Essam <122894217+Adel-Essam@users.noreply.github.com>
2023-10-04 14:09:29 +0000

    Wed, Oct 4, 2023, 4:09 PM +02:00

--- readability/readability.c
+++ readability/readability.c
@@ -11,21 +11,24 @@ int main(void)
 {
     string s = get_string("text: ");
     float l = count_letters(s), w = count_words(s), se = count_sentences(s);
+    printf("letters: %f", l)
+    printf("words: %f", w)
+    printf("sentenses: %f", se)
     float l2 = (l / w) * 100;
     float se2 = (se / w) * 100;
 
-    if (((l2 * 0.0588) - (se2 * 0.296) - 15.8) < 1)
-    {
-        printf("Before Grade 1\n");
-    }
-    else if (((l2 * 0.0588) - (se2 * 0.296) - 15.8) > 16)
-    {
-        printf("Grade 16+\n");
-    }
-    else
-    {
-        printf("Grade %.0f\n", ((l2 * 0.0588) - (se2 * 0.296) - 15.8));
-    }
+    // if (((l2 * 0.0588) - (se2 * 0.296) - 15.8) < 1)
+    // {
+    //     printf("Before Grade 1\n");
+    // }
+    // else if (((l2 * 0.0588) - (se2 * 0.296) - 15.8) > 16)
+    // {
+    //     printf("Grade 16+\n");
+    // }
+    // else
+    // {
+    //     printf("Grade %.0f\n", ((l2 * 0.0588) - (se2 * 0.296) - 15.8));
+    // }
 }
 int count_sentences(string s)
 {
