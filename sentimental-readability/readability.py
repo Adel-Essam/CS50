862e944b5af64d505718c5891efa8a9d68bc983f
Adel Essam <122894217+Adel-Essam@users.noreply.github.com>
2023-10-04 14:33:02 +0000

    Wed, Oct 4, 2023, 4:33 PM +02:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,5 +1,22 @@
 # TODO
 def main():
-    ...
+    s = input("Text: ")
+    se = 0
+    l = 0
+    for i in s :
+        if i.isalnum():
+            l += 1
+        if i == '.' or i == '!' or i == '?':
+            se += 1
+    s = s.split() # len(s)
+    w = len(s)
+    l = (l / w) * 100
+    se = (se / w) * 100
+    if ((l * 0.0588) - (se * 0.296) - 15.8) < 1:
+        print("Before grade 1")
+    elif ((l * 0.0588) - (se * 0.296) - 15.8) > 16:
+        print("Grade 16+")
+    else:
+        print(f"Grade {(l * 0.0588) - (se * 0.296) - 15.8 :.0f}")
 
 main()
